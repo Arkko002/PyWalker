539678e2e03112800f05e8e3eb89ffbc2ff7a722
arkko <arkko002@gmail.com>
2021-07-13 10:54:38 +0200

    Add dropbox authentication link to export page

--- src/front-react/src/components/export/export.tsx
+++ src/front-react/src/components/export/export.tsx
@@ -9,7 +9,7 @@ function Export(): React.ReactElement {
             <div className="flex flex-col px-3 py-2 m-4 bg-white">
                 <span>Here you can link your Dropbox account to automatically recive scraping result uploads to a separate <b>WebScrapper</b> folder. We will only have access to this folder and not your entire Dropbox account.</span>
 
-                        <input className="p-2 mt-2 font-bold text-white bg-blue-500 rounded-sm hover:bg-blue-700 focus:shadow-inner" type="submit" value="Link your account"/>
+                <a href="https://www.dropbox.com/oauth2/authorize?client_id=2175k18uazdwzb4&response_type=code" className="p-2 mt-2 font-bold text-white bg-blue-500 rounded-sm hover:bg-blue-700 focus:shadow-inner">Link your accout</a>
             </div>
 
         </div>
